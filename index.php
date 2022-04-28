b7479f0cac6eea522d77531c4b7fdf28eb65c89f
Sergei Brezitskii <90458734+SergeiB2002@users.noreply.github.com>
2022-04-28 19:57:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,58 +1,94 @@
 <?php
-/**
- * Реализовать возможность входа с паролем и логином с использованием
- * сессии для изменения отправленных данных в предыдущей задаче,
- * пароль и логин генерируются автоматически при первоначальной отправке формы.
- */
-
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
+session_start();
 header('Content-Type: text/html; charset=UTF-8');
-
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // Массив для временного хранения сообщений пользователю.
   $messages = array();
-
-  // В суперглобальном массиве $_COOKIE PHP хранит все имена и значения куки текущего запроса.
-  // Выдаем сообщение об успешном сохранении.
   if (!empty($_COOKIE['save'])) {
-    // Удаляем куку, указывая время устаревания в прошлом.
     setcookie('save', '', 100000);
     setcookie('login', '', 100000);
-    setcookie('pass', '', 100000);
-    // Выводим сообщение пользователю.
+    setcookie('sign_in', '', 100000);
     $messages[] = 'Спасибо, результаты сохранены.';
-    // Если в куках есть пароль, то выводим сообщение.
-    if (!empty($_COOKIE['pass'])) {
-      $messages[] = sprintf('Вы можете <a href="login.php">войти</a> с логином <strong>%s</strong>
-        и паролем <strong>%s</strong> для изменения данных.',
+    if (!empty($_COOKIE['sign_in'])) {
+      $messages[] = sprintf('Войдите <a href="login.php">войти</a> с логином <strong>%s</strong>
+        и паролем <strong>%s</strong> чтобы измененить данные.',
         strip_tags($_COOKIE['login']),
-        strip_tags($_COOKIE['pass']));
+        strip_tags($_COOKIE['sign_in']));
     }
+    setcookie('name_value', '', 100000);
+    setcookie('email_value', '', 100000);
+    setcookie('year_value', '', 100000);
+    setcookie('pol_value', '', 100000);
+    setcookie('limb_value', '', 100000);
+    setcookie('bio_value', '', 100000);
+    setcookie('power_value', '', 100000);
+    setcookie('telepat_value', '', 100000);
+    setcookie('noclip_value', '', 100000);
+    setcookie('immortal_value', '', 100000);
+    setcookie('check_value', '', 100000);
   }
 
-  // Складываем признак ошибок в массив.
   $errors = array();
-  $errors['fio'] = !empty($_COOKIE['fio_error']);
-
-  // TODO: аналогично все поля.
-
-  // Выдаем сообщения об ошибках.
-  if (!empty($errors['fio'])) {
-    // Удаляем куку, указывая время устаревания в прошлом.
-    setcookie('fio_error', '', 100000);
-    // Выводим сообщение.
-    $messages[] = '<div class="error">Заполните имя.</div>';
+  $error=FALSE;
+  $errors['field-name'] = !empty($_COOKIE['name_error']);
+  $errors['field-email'] = !empty($_COOKIE['email_error']);
+  $errors['year'] = !empty($_COOKIE['year_error']);
+  $errors['radio-pol'] = !empty($_COOKIE['pol_error']);
+  $errors['radio-limb'] = !empty($_COOKIE['limb_error']);
+  $errors['field-super'] = !empty($_COOKIE['super_error']);
+  $errors['field-bio'] = !empty($_COOKIE['bio_error']);
+  $errors['checkbox'] = !empty($_COOKIE['check_error']);
+  if ($errors['field-name']) {
+    setcookie('name_error', '', 100000);
+    $messages[] = '<div class="error">Заполните имя или у него неверный формат (only English)</div>';
+    $error=TRUE;
   }
-  // TODO: тут выдать сообщения об ошибках в других полях.
-
-  // Складываем предыдущие значения полей в массив, если есть.
-  // При этом санитизуем все данные для безопасного отображения в браузере.
+  if ($errors['field-email']) {
+    setcookie('email_error', '', 100000);
+    $messages[] = '<div class="error">Заполните имейл или у него неверный формат</div>';
+    $error=TRUE;
+  }
+  if ($errors['year']) {
+    setcookie('year_error', '', 100000);
+    $messages[] = '<div class="error">Выберите год.</div>';
+    $error=TRUE;
+  }
+  if ($errors['radio-pol']) {
+    setcookie('pol_error', '', 100000);
+    $messages[] = '<div class="error">Выберите пол.</div>';
+    $error=TRUE;
+  }
+  if ($errors['radio-limb']) {
+    setcookie('limb_error', '', 100000);
+    $messages[] = '<div class="error">Укажите кол-во конечностей.</div>';
+    $error=TRUE;
+  }
+  if ($errors['field-super']) {
+    setcookie('super_error', '', 100000);
+    $messages[] = '<div class="error">Выберите суперспособности(хотя бы одну).</div>';
+    $error=TRUE;
+  }
+  if ($errors['field-bio']) {
+    setcookie('bio_error', '', 100000);
+    $messages[] = '<div class="error">Заполните биографию или у неё неверный формат (only English)</div>';
+    $error=TRUE;
+  }
+  if ($errors['checkbox']) {
+    setcookie('check_error', '', 100000);
+    $messages[] = '<div class="error">Вы должны болеть за Red Bull Racing.</div>';
+    $error=TRUE;
+  }
   $values = array();
-  $values['fio'] = empty($_COOKIE['fio_value']) ? '' : strip_tags($_COOKIE['fio_value']);
-  // TODO: аналогично все поля.
+  $values['field-name'] = empty($_COOKIE['name_value']) ? '' : strip_tags($_COOKIE['name_value']);
+  $values['field-email'] = empty($_COOKIE['email_value']) ? '' : strip_tags($_COOKIE['email_value']);
+  $values['year'] = empty($_COOKIE['year_value']) ? 0 : $_COOKIE['year_value'];
+  $values['radio-pol'] = empty($_COOKIE['pol_value']) ? '' : $_COOKIE['pol_value'];
+  $values['radio-limb'] = empty($_COOKIE['limb_value']) ? '' : $_COOKIE['limb_value'];
+  $values['immortal'] = empty($_COOKIE['immortal_value']) ? 0 : $_COOKIE['immortal_value'];
+  $values['noclip'] = empty($_COOKIE['noclip_value']) ? 0 : $_COOKIE['noclip_value'];
+  $values['power'] = empty($_COOKIE['power_value']) ? 0 : $_COOKIE['power_value'];
+  $values['telepat'] = empty($_COOKIE['telepat_value']) ? 0 : $_COOKIE['telepat_value'];
+  $values['field-bio'] = empty($_COOKIE['bio_value']) ? '' : strip_tags($_COOKIE['bio_value']);
+  $values['checkbox'] = empty($_COOKIE['check_value']) ? FALSE : $_COOKIE['check_value'];
 
   // Если нет предыдущих ошибок ввода, есть кука сессии, начали сессию и
   // ранее в сессию записан факт успешного логина.
